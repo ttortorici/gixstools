702bf5f60aac7e1bbc2dec978eae37fdfe904ef9
Teddy <etc.tortorici@gmail.com>
2025-06-08 09:59:47 -0600

    a

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Grazing Incidence X-ray Scattering Tools'
+copyright = '2025, Edward Tortorici'
+author = 'Edward Tortorici'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
