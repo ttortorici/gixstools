d45da37c43196a0190884d28fe199ea9b2e9207e
Teddy <etc.tortorici@gmail.com>
2025-06-08 20:54:54 -0600

    a

--- gixstools/__init__.py
+++ gixstools/__init__.py
@@ -2,5 +2,5 @@
 # from . import align
 # from . import _programs
 
-if False:  # or use an env var like `if os.environ.get("SPHINX_BUILD")`
-    from . import align  # Makes it discoverable to Sphinx
+# if False:  # or use an env var like `if os.environ.get("SPHINX_BUILD")`
+#     from . import align  # Makes it discoverable to Sphinx
